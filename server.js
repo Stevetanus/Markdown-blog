939dcff3ccbac8d2a12909027272b393c2e2d912
Steven Wang <steventhibe@gmail.com>
2022-04-07 21:49:28 +0800

    First commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Article = require("./models/article");
+const articleRouter = require("./routes/articles");
+const methodOverride = require("method-override");
+const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connect to Mongo Atlas.");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: "desc" });
+  res.render("articles/index", { articles: articles });
+});
+
+app.use("/articles", articleRouter);
+app.listen(5000);
